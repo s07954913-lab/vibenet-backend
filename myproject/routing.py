e0f2c6c631ee1e5e19315f13638a0833addcc2e4
s07954913 <s07954913@gmail.com>
2026-06-14 17:09:02 -0700

    Fix: WebSocket routing allow special chars in room_name

--- myproject/routing.py
+++ myproject/routing.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 from activity import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$',        consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/comments/(?P<post_id>\d+)/$',      consumers.CommentConsumer.as_asgi()),
-    re_path(r'ws/notifications/(?P<user_id>[\w-]+)/$', consumers.NotificationConsumer.as_asgi()),
-    re_path(r'ws/api-logs/$',                        consumers.APILogConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>[\w\-]+)/$',        consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/comments/(?P<post_id>\d+)/$',          consumers.CommentConsumer.as_asgi()),
+    re_path(r'ws/notifications/(?P<user_id>[\w\-]+)/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/api-logs/$',                            consumers.APILogConsumer.as_asgi()),
 ]
